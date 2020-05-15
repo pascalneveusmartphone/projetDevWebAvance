681a03822a298344b7d3cdd3136d033ccdc4cbb7
pascalneveusmartphone <pascal.neveu.smartphone@gmail.com>
2020-05-15 13:13:22 +0200

    Modif

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,32 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-// import { ContactFormComponent } from './contact-form/contact-form.component';
 import { TodosComponent } from './todos/todos.component';
-// import { RouterFormComponent } from './router-form/router-form.component';
-// import { HomeComponent } from './router-form/views/home/home.component';
-// import { AdminComponent } from './router-form/views/admin/admin.component';
 import { RouterModule } from '@angular/router';
 import { TodoFormComponent } from './todo-form/todo-form.component';
-// import { ROUTES } from './app.routes';
-//import { Contacts2Component } from './contacts2/contacts2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    // ContactFormComponent,
     TodosComponent,
     TodoFormComponent,
-    /*RouterFormComponent,
-    HomeComponent,
-    AdminComponent,
-    Contacts2Component*/
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    //RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
